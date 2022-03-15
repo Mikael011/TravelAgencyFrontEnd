ae411fec88d149ae79452637eae0ca825015d709
arpiantal <arpiantal1974@gmail.com>
2022-03-16 00:07:10 +0200

    Revert "added hotel and reservation - Service, DTO and Model"
    This reverts commit 84845101f88650b3ad2ff87f0549f652792515e2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,13 @@ import { RoomListComponent } from './room-list/room-list.component';
 import {HttpClientModule} from "@angular/common/http";
 import { AirportListComponent } from './airport-list/airport-list.component';
 import { TicketListComponent } from './ticket-list/ticket-list.component';
-import { HotelListComponent } from './hotel-list/hotel-list.component';
-import { ReservationListComponent } from './reservation-list/reservation-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RoomListComponent,
     AirportListComponent,
-    TicketListComponent,
-    HotelListComponent,
-    ReservationListComponent,
+    TicketListComponent
   ],
   imports: [
     BrowserModule,
