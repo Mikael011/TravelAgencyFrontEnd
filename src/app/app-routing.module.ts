e5b712ec129666b74086d0680decbc924a8526ed
arpiantal <arpiantal1974@gmail.com>
2022-03-17 11:09:21 +0200

    added paths ->app-routing.module.ts, & component ->app.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AirportListComponent} from "./airport-list/airport-list.component";
+import {CityListComponent} from "./city-list/city-list.component";
+import {CountryListComponent} from "./country-list/country-list.component";
+import {HotelListComponent} from "./hotel-list/hotel-list.component";
+import {ReservationListComponent} from "./reservation-list/reservation-list.component";
 import {RoomListComponent} from "./room-list/room-list.component";
+import {TicketListComponent} from "./ticket-list/ticket-list.component";
 
 const routes: Routes = [
-  {path: 'rooms', component: RoomListComponent}
+  {path: 'airports', component: AirportListComponent},
+  {path: 'cities', component: CityListComponent},
+  {path: 'countries', component: CountryListComponent},
+  {path: 'hotels', component: HotelListComponent},
+  {path: 'reservations', component: ReservationListComponent},
+  {path: 'rooms', component: RoomListComponent},
+  {path: 'tickets', component: TicketListComponent},
 ];
 
 @NgModule({
