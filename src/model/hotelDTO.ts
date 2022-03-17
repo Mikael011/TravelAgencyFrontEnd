fa3ad5dbc24cb6cc61478b5a1b3e853efb61839c
arpiantal <arpiantal1974@gmail.com>
2022-03-17 16:19:35 +0200

    create buttons

--- src/model/hotelDTO.ts
+++ src/model/hotelDTO.ts
@@ -1,9 +1,13 @@
 export class HotelDTO {
   name: string;
+  numberOfStars: number;
+  description: string;
   cityId: number;
 
-  constructor(name: string, cityId: number) {
+  constructor(name: string, numberOfStars: number, descriptions: string, cityId: number) {
     this.name = name;
+    this.numberOfStars = numberOfStars;
+    this.description = descriptions;
     this.cityId = cityId;
   }
 
